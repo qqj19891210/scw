03aba8f6fb073083a1bdd90aee6cd2b57bfe0395
qin <283271603@qq.com>
2018-08-28 22:08:16 +0800

    第一次修改

--- scw-manager/manager-service/src/main/java/com/smart/scw/manager/service/UserService.java
+++ scw-manager/manager-service/src/main/java/com/smart/scw/manager/service/UserService.java
@@ -1,7 +1,5 @@
 package com.smart.scw.manager.service;
 
-import com.smart.scw.manager.bean.User;
-
 public interface UserService {
 
     public User getUserById(Integer id);
--- scw-manager/manager-service/src/main/java/com/smart/scw/manager/service/impl/UserServiceImpl.java
+++ scw-manager/manager-service/src/main/java/com/smart/scw/manager/service/impl/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package com.smart.scw.manager.service.impl;
 
-import com.smart.scw.manager.bean.User;
-import com.smart.scw.manager.dao.UserMapper;
 import com.smart.scw.manager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;

4b4a61a4edf8af0f2961d2d82477097a23881fd1
qin <283271603@qq.com>
2018-08-31 13:01:49 +0800

    注册功能完成

--- project-commons/src/main/java/com/smart/project/MyStringUtils.java
+++ project-commons/src/main/java/com/smart/project/MyStringUtils.java
@@ -1,12 +1,21 @@
 package com.smart.project;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MyStringUtils {
 
-    public static boolean isEmpty(String str){
-        if(str==null){
+    //yyyy-MM-dd HH:mm:ss 把日期转为字符串
+    public static String formatDate(Date date) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return simpleDateFormat.format(date);
+    }
+
+    public static boolean isEmpty(String str) {
+        if (str == null) {
             return true;
         }
-        if(str.trim().equals("")){
+        if (str.trim().equals("")) {
             return true;
         }
         return false;
